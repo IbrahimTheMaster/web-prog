e04dba9bcde3b4f7e2fdc4698107cee08684d882
IbrahimTheMaster <aliyuibraheem21@mail.com>
2026-04-09 11:10:12 +0200

    Build main page media showcase.

--- templates/pages/home.tpl.php
+++ templates/pages/home.tpl.php
@@ -1,11 +1,43 @@
-<h2>Welcome</h2>
-<img src="./images/face.jpg">
-<h3>What is Lorem Ipsum?</h3>
-<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
-<h3>Where does it come from?</h3>
-<p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.</p>
-<p>The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for those interested. Sections 1.10.32 and 1.10.33 from "de Finibus Bonorum et Malorum" by Cicero are also reproduced in their exact original form, accompanied by English versions from the 1914 translation by H. Rackham.</p>
-<h3>Why do we use it?</h3>
-<p>It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).</p>
-<h3>Where can I get some?</h3>
-<p>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
+<article class="main-intro">
+    <h2>Welcome to City Explorer Portal</h2>
+    <img src="./images/face.jpg" alt="City skyline illustration">
+    <p>
+        City Explorer Portal is a colourful showcase for local attractions, public spaces, and hidden
+        neighborhood gems. This project demonstrates how a front-controller PHP app can present practical
+        city information in a friendly and responsive interface.
+    </p>
+    <p>
+        The website theme connects directly to our chosen data source and will later include searchable records,
+        image uploads, messages, and CRUD operations for city-related content.
+    </p>
+</article>
+
+<section class="media-section">
+    <h3>Local Video (5 seconds)</h3>
+    <video controls preload="metadata" width="560">
+        <source src="./images/local-city-clip-5s.mp4" type="video/mp4">
+        Your browser does not support the video tag.
+    </video>
+</section>
+
+<section class="media-section">
+    <h3>YouTube Highlight</h3>
+    <iframe width="560" height="315"
+        src="https://www.youtube.com/embed/21X5lGlDOfg"
+        title="YouTube city highlight video"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        allowfullscreen></iframe>
+</section>
+
+<section class="media-section">
+    <h3>Find Us on the Map</h3>
+    <iframe
+        src="https://www.google.com/maps?q=Budapest%20Parliament&output=embed"
+        width="560"
+        height="320"
+        style="border:0;"
+        allowfullscreen
+        loading="lazy"
+        referrerpolicy="no-referrer-when-downgrade"></iframe>
+</section>
